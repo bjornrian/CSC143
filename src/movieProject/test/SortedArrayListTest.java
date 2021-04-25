daf49ea876d58d318b0de9db2dedb59f174533c6
Egil Rian <egil.rian@expeditors.com>
2021-04-25 14:42:56 -0700

    SortedArrayListTest.testAdd needs to be fixed when two movies are added

--- src/movieProject/test/SortedArrayListTest.java
+++ src/movieProject/test/SortedArrayListTest.java
@@ -14,12 +14,17 @@ public class SortedArrayListTest {
         Movie aladdin = new Movie("Aladdin");
         list.add(aladdin);
         assertEquals(1, list.size());
-        Movie movieAtZeroIndex = (Movie) list.get(0);
-        assertEquals(aladdin.getTitle(), movieAtZeroIndex.getTitle());
+        Movie movieAtIndexZero = (Movie) list.get(0);
+        assertEquals(aladdin.getTitle(), movieAtIndexZero.getTitle());
 
         Movie starTrek = new Movie("Star Trek");
         list.add(starTrek);
         assertEquals(2, list.size());
+
+        movieAtIndexZero = (Movie) list.get(0);
+        assertEquals(aladdin.getTitle(), movieAtIndexZero.getTitle());
+        Movie movieAtIndexOne = (Movie) list.get(1);
+        assertEquals(starTrek.getTitle(), movieAtIndexOne.getTitle());
 
         list.toString();
     }
