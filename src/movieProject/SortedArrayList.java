29066b59af9dad6d64aea553713231b58aff3c76
Egil Rian <egil.rian@expeditors.com>
2021-04-25 16:23:58 -0700

    SortedArrayListTest added tests for indexof(), contains()

--- src/movieProject/SortedArrayList.java
+++ src/movieProject/SortedArrayList.java
@@ -5,7 +5,7 @@ import java.util.Iterator;
 
 public class SortedArrayList<E extends Comparable<E>> {
     private ArrayList<E> list;
-    private int numberOfMovies = 0;
+    private int numberOfElements = 0;
 
     public SortedArrayList() {
         list = new ArrayList<>();
@@ -16,43 +16,19 @@ public class SortedArrayList<E extends Comparable<E>> {
     }
 
     public void add(E value) {
-        if(numberOfMovies == 0) {
+        if(numberOfElements == 0) {
             list.add(value);
         } else {
             //use binary search to find the index where we want to insert our object
-            int targetIndex = binarySearch(list, value);
+            int targetIndex = binarySearch(value);
             list.add(targetIndex, value);
         }
-        numberOfMovies++;
+        numberOfElements++;
     }
 
-    /**
-     * Search through the list until we find the target index where we want to insert the new element.
-     *
-     * @param list
-     * @param value
-     * @return
-     */
-    //list = string list
-    //value = target
-    private int binarySearch(ArrayList<E> list, E value) {
-        int min = 0;
-        int max = list.size() - 1;
-        while (min <= max) {
-            int mid = (min + max) / 2;
-            if (list.get(mid).compareTo(value) < 0) {
-                min = mid + 1;
-            } else if (list.get(mid).compareTo(value) > 0) {
-                max = mid - 1;
-            } else {
-                return mid; // target found
-            }
-        }
-        return min; // target not found
-    }
-
-    public void indexOf(E value) {
-        
+    //todo ask Barry if void return type (in UML) is correct
+    public int indexOf(E value) {
+        return 0;
     }
 
     public Boolean contains(E value) {
@@ -64,7 +40,7 @@ public class SortedArrayList<E extends Comparable<E>> {
     }
 
     public int size() {
-        return numberOfMovies;
+        return numberOfElements;
     }
 
     public Boolean isEmpty() {
@@ -94,5 +70,27 @@ public class SortedArrayList<E extends Comparable<E>> {
             System.out.println(movie.toString());
         }
         return null;
+    }
+
+    /**
+     * Search through the list until we find the target index where we want to insert the new element.
+     *
+     * @param value
+     * @return
+     */
+    private int binarySearch(E value) {
+        int min = 0;
+        int max = list.size() - 1;
+        while (min <= max) {
+            int mid = (min + max) / 2;
+            if (list.get(mid).compareTo(value) < 0) {
+                min = mid + 1;
+            } else if (list.get(mid).compareTo(value) > 0) {
+                max = mid - 1;
+            } else {
+                return mid; // target found
+            }
+        }
+        return min; // target not found
     }
 }
--- src/movieProject/test/SortedArrayListTest.java
+++ src/movieProject/test/SortedArrayListTest.java
@@ -7,36 +7,68 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class SortedArrayListTest {
+
+    private static final String ALADDIN = "Aladdin";
+    private static final String STAR_TREK = "Star Trek";
+    private static final String BAR_TIME = "Bar Time";
+    private static final String DARK_SIDE = "Dark Side";
+    private static final String ABRACADABRA = "Abracadabra";
+
     @Test
-    public void testAdd() {
+    public void testAddSingleMovie() {
         SortedArrayList list = new SortedArrayList();
         assertEquals(0, list.size());
-        Movie aladdin = new Movie("Aladdin");
+        Movie aladdin = new Movie(ALADDIN);
         list.add(aladdin);
         assertEquals(1, list.size());
         Movie movieAtIndexZero = (Movie) list.get(0);
         assertEquals(aladdin.getTitle(), movieAtIndexZero.getTitle());
+        list.toString();
+    }
 
-        Movie starTrek = new Movie("Star Trek");
-        list.add(starTrek);
-        assertEquals(2, list.size());
-        Movie barTime = new Movie("Bar Time");
-        list.add(barTime);
-        assertEquals(3, list.size());
-        Movie darkSide = new Movie("Dark Side");
-        list.add(darkSide);
-        assertEquals(4, list.size());
-
-
-        movieAtIndexZero = (Movie) list.get(0);
-        assertEquals(aladdin.getTitle(), movieAtIndexZero.getTitle());
-        Movie movieAtIndexOne = (Movie) list.get(1);
-        assertEquals(barTime.getTitle(), movieAtIndexOne.getTitle());
-        Movie movieAtIndexTwo = (Movie) list.get(2);
-        assertEquals(darkSide.getTitle(), movieAtIndexTwo.getTitle());
-        Movie movieAtIndexThree = (Movie) list.get(3);
-        assertEquals(starTrek.getTitle(), movieAtIndexThree.getTitle());
+    @Test
+    public void testAddMultipleMovies() {
+        SortedArrayList list = buildListWithMultipleMovies();
+        assertEquals(5, list.size());
+        assertEquals(ABRACADABRA, ((Movie) list.get(0)).getTitle());
+        assertEquals(ALADDIN, ((Movie) list.get(1)).getTitle());
+        assertEquals(BAR_TIME, ((Movie) list.get(2)).getTitle());
+        assertEquals(DARK_SIDE, ((Movie) list.get(3)).getTitle());
+        assertEquals(STAR_TREK, ((Movie) list.get(4)).getTitle());
 
         list.toString();
+    }
+
+    /**
+     * Build a list of multiple movies and make sure they are positioned at the correct index
+     */
+    @Test
+    public void testIndexOf() {
+        //todo
+    }
+
+    /**
+     * Build a list of multiple movies and make sure the contains method works correctly for
+     * existing movies as well as non-existing movies
+     */
+    @Test
+    public void testContains() {
+        //todo
+    }
+
+    private SortedArrayList buildListWithMultipleMovies() {
+        SortedArrayList list = new SortedArrayList();
+        Movie aladdin = new Movie(ALADDIN);
+        list.add(aladdin);
+        Movie starTrek = new Movie(STAR_TREK);
+        list.add(starTrek);
+        Movie barTime = new Movie(BAR_TIME);
+        list.add(barTime);
+        Movie darkSide = new Movie(DARK_SIDE);
+        list.add(darkSide);
+        Movie abracadabra = new Movie(ABRACADABRA);
+        list.add(abracadabra);
+
+        return list;
     }
 }
