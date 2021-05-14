b2f49b6b817c90bf620dd978a46a5767a4702221
Bjorn Rian <bjornrian@gmail.com>
2021-05-14 11:40:16 -0700

    Tests for Wearable.class finished, just accessors in that class.

--- src/wearableDevices/tests/TestWearable.java
+++ src/wearableDevices/tests/TestWearable.java
@@ -1,4 +1,27 @@
 package wearableDevices.tests;
 
+import org.junit.Test;
+import wearableDevices.classes.Wearable;
+
+import static org.junit.Assert.*;
+
 public class TestWearable {
+    @Test
+    public void testAccessors() {
+        Wearable testWearable = new Wearable(133, "FitBit", 55.99,
+                "Wrist", "Athletics", "Barthas",
+                "www.barthas.com/fitbit", "Rabat, Morocco",
+                "Rabat", null, "Morocco");
+        assertEquals(133, testWearable.getRanking());
+        assertEquals("FitBit", testWearable.getName());
+        assertEquals(55.99, testWearable.getPrice(), 0.001);
+        assertEquals("Wrist", testWearable.getBodyLocation());
+        assertEquals("Athletics", testWearable.getCategory());
+        assertEquals("Barthas", testWearable.getCompanyName());
+        assertEquals("www.barthas.com/fitbit", testWearable.getCompanyURL());
+        assertEquals("Rabat, Morocco", testWearable.getCompanyMappingLocation());
+        assertEquals("Rabat", testWearable.getCompanyCity());
+        assertNull(testWearable.getCompanyUSState());
+        assertEquals("Morocco", testWearable.getCompanyCountry());
+    }
 }
