195f08b628a5fa0d62e75f0f9458b6410bd2d371
Bjorn Rian <bjornrian@gmail.com>
2021-06-15 18:45:13 -0700

    First fractal part is now painted

--- src/tetheredFractals/MyFrame.java
+++ src/tetheredFractals/MyFrame.java
@@ -7,6 +7,10 @@ import java.awt.event.ActionListener;
 
 public class MyFrame extends JFrame implements ActionListener {
     private Toolkit toolkit;
+    public Integer DIAMETER = 100;
+    public Integer RADIUS = 50;
+    public Integer START_X = 480;
+    public Integer START_Y = 540;
     private Integer numberOfChildren = 3;
     private Integer childParentRatio = 40;
     private Integer recursionDepth = 5;
@@ -20,7 +24,6 @@ public class MyFrame extends JFrame implements ActionListener {
         this.setTitle("Tethered Radial Fractal");
         this.setResizable(false);
         this.setLayout(null);
-        this.setVisible(true);
         this.setDefaultCloseOperation(EXIT_ON_CLOSE);
         toolkit = getToolkit();
 
@@ -34,7 +37,8 @@ public class MyFrame extends JFrame implements ActionListener {
         //JButtons
         JComboBox<Integer> numberOfChildrenSelector = new JComboBox<>(new Integer[]
                 {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13});
-        numberOfChildrenSelector.setBounds(60, 120, 300, 60 );
+        numberOfChildrenSelector.setBounds(60, 120, 300, 60);
+        numberOfChildrenSelector.setSelectedIndex(2);
         numberOfChildrenSelector.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -49,7 +53,8 @@ public class MyFrame extends JFrame implements ActionListener {
                 {20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37,
                         38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53,
                         54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70});
-        childToParentRatioSelector.setBounds(60, 267, 300, 60 );
+        childToParentRatioSelector.setBounds(60, 267, 300, 60);
+        childToParentRatioSelector.setSelectedIndex(20);
         childToParentRatioSelector.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -62,7 +67,8 @@ public class MyFrame extends JFrame implements ActionListener {
 
         JComboBox<Integer> recursionDepthSelector = new JComboBox<>(new Integer[]
                 {2, 3, 4, 5, 6, 7, 8, 9, 10});
-        recursionDepthSelector.setBounds(60, 416, 300, 60 );
+        recursionDepthSelector.setBounds(60, 416, 300, 60);
+        recursionDepthSelector.setSelectedIndex(3);
         recursionDepthSelector.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -79,6 +85,7 @@ public class MyFrame extends JFrame implements ActionListener {
             @Override
             public void actionPerformed(ActionEvent e) {
                 JFrame colorFrame = new JFrame();
+                colorFrame.setLocation(0, 540);
                 colorFrame.setSize(960, 540);
                 colorFrame.setTitle("Color Chooser");
                 colorFrame.setResizable(false);
@@ -105,10 +112,9 @@ public class MyFrame extends JFrame implements ActionListener {
             @Override
             public void actionPerformed(ActionEvent e) {
                 JComboBox source = (JComboBox) e.getSource();
-                if(source.getSelectedItem().equals("White")) {
+                if (source.getSelectedItem().equals("White")) {
                     backgroundColor = Color.WHITE;
-                }
-                else {
+                } else {
                     backgroundColor = Color.BLACK;
                 }
 
@@ -139,9 +145,8 @@ public class MyFrame extends JFrame implements ActionListener {
                 drawingPanel.setLayout(null);
                 drawingFrame.add(drawingPanel);
                 drawingPanel.setVisible(true);
+
                 System.out.println();
-
-
                 System.out.println("Child Count: " + numberOfChildren);
                 System.out.println("Ratio: " + childParentRatio + "%");
                 System.out.println("Recursion Depth: " + recursionDepth);
@@ -150,6 +155,9 @@ public class MyFrame extends JFrame implements ActionListener {
             }
         });
         widgetPanel.add(drawSelector);
+
+        //setting visibility at the end removes the bug of buttons randomly appearing
+        this.setVisible(true);
     }
 
     @Override
@@ -161,8 +169,31 @@ public class MyFrame extends JFrame implements ActionListener {
         @Override
         public void paintComponent(Graphics g) {
             super.paintComponent(g);
-            g.drawRect(96, 96, 390, 390);
-            g.setColor(Color.BLACK);
+            g.setColor(color);
+            //patient zero
+            g.drawOval(START_X - 50, START_Y - 50, DIAMETER, DIAMETER);
+            paintShape(g, recursionDepth);
+        }
+
+        public void paintShape(Graphics g, Integer depth) {
+            if(depth > 1) {
+                depth--;
+                for(int i = 1; i <= numberOfChildren; i++) {
+                    double piValue = (i*2*Math.PI)/(numberOfChildren);
+                    //children
+                    g.drawOval((int) (DIAMETER * Math.cos(piValue) + START_X - (DIAMETER / 2 * childParentRatio / 100)),
+                            (int) (DIAMETER * Math.sin(piValue) + START_Y - (DIAMETER / 2 * childParentRatio / 100)),
+                            DIAMETER * childParentRatio / 100,
+                            DIAMETER * childParentRatio / 100);
+
+                    //parent to child lines
+                    g.drawLine((int) ((DIAMETER - (DIAMETER / 2 * childParentRatio / 100)) * Math.cos(piValue) + START_X),
+                            (int) ((DIAMETER - (DIAMETER / 2 * childParentRatio / 100)) * Math.sin(piValue) + START_Y),
+                            (int) (DIAMETER / 2 * Math.cos(piValue) + START_X),
+                            (int) (DIAMETER / 2 * Math.sin(piValue) + START_Y));
+                }
+                paintShape(g, depth);
+            }
         }
     }
 }
